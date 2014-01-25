1b14771646cc03e5d6c98967ae5545abaf081a6f
Josh <ogunyinkajoshua@yahoo.com>
2014-01-25 13:24:11 -0800

    Update memory.h
    Updated the templates to contain the full definition of unique_ptrs

--- memory.h
+++ memory.h
@@ -1,3 +1,4 @@
+
 #ifndef MEMORY_H
 #define MEMORY_H
 
@@ -5,18 +6,36 @@ template<typename T>
 class Unique_ptr
 {
 public:
-    Unique_ptr<T>(const Unique_ptr<T> &) = delete;
+  Unique_ptr<T>(const Unique_ptr<T> &) = delete;
 	Unique_ptr<T>& operator=(const Unique_ptr &) = delete;
-	Unique_ptr<T>(T *);
-	~Unique_ptr();
+	Unique_ptr<T>(T *p = nullptr): data(p) { }
+	~Unique_ptr(){
+		free();
+	}
 	
-	T* release();
-	void reset(T *);
-	T& operator*();
+	T *release(){
+		T *copy = data;
+		data = nullptr;
+		return copy;
+	}
+	
+	void reset(T *p = nullptr){
+		free(p);
+	}
+	
+	T& operator*(){
+		return *data;
+	}
 	
 private:
-  void free(T *);
-  T * data;
+    void free(T *p = nullptr){
+		if(data){
+		  delete data;
+		}
+		data = p;
+		}
+		
+    T * data;
 };
 
 #endif
