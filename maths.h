1ac2ca333ea2ccd82d45afca8ec66145a61e22b5
Josh <ogunyinkajoshua@yahoo.com>
2014-03-20 12:47:25 +0800

    Update maths.h

--- maths.h
+++ maths.h
@@ -7,7 +7,7 @@ namespace maths
 	template <class T>
 	using vector = vector<T>;
 	
-        template<class T>
+	template<class T>
     using container = typename std::vector<T>;
     using svec = container<std::string>;
     using ivec = container<int>;
@@ -15,6 +15,27 @@ namespace maths
     using pair_is = typename std::pair<int, std::string>; 
     using vec_pairs = container<pair_is>;
     
+    struct Less
+    {
+		template<class T>
+		bool operator()(T &&a, T &&b)
+		{
+			return std::tie(a) < std::tie(b);
+		}
+		template<typename T, typename U>
+		bool operator()(T &&a, U &&b)
+		{
+			return a < b;
+		}
+	};
+	template<class T, class U = std::less<T>>
+	int compare(const T &a, const T &b, U func){
+	    return func(a, b);
+	}	
+	template<class T, class U = std::less<T>>
+	int compare(const T &a, const T &b){
+	    return U()(a, b);
+	}
 	template<typename T>
 	void cprint(const T &c, char sep = ' '){
 		typename T::const_iterator begin = c.cbegin(), end = c.cend();
@@ -22,7 +43,26 @@ namespace maths
 			std::cout << *i << sep;
 		}
 	}
-	
+	template<typename Tuple, size_t N>
+	struct TuplePrinter
+	{
+		void print(const Tuple &arg){
+			TuplePrinter<Tuple, N-1>::print(arg);
+			std::cout << std::get<N-1>(arg) << " ";
+		}
+	};
+	template<typename Tuple>
+	struct TuplePrinter<Tuple, 1>
+	{
+		void print(const Tuple &arg){
+			std::cout << std::get<0>(arg) << " " ;
+		}
+	};
+	template<typename ...T>
+	void tuple_print(const std::tuple<T...> &arg)
+	{
+		TuplePrinter<decltype(arg), sizeof...(T)>::print(arg);
+	}
 	template<typename T>
 	void cprint(const T &c, const std::string &sep){
 		typename T::const_iterator begin = c.cbegin(), end = c.cend();
@@ -30,23 +70,23 @@ namespace maths
 			std::cout << *i << sep;
 		}
 	}
-	
+	template<typename T, typename U>
+	void cprint(const std::vector<std::pair<T, U>> &vecsi, char sep = ' ')
+	{
+		for(const auto &i: vecsi){
+			std::cout << i.first << ' ' << i.second << sep;
+		}
+	}
 	template<typename T>
 	void print(T &&c, char sep = ' '){
 		std::cout << c << sep;
 	}
-	
-	//this part is implemented by Cameron DaCamara, NOT me!
 	template<class T, class U, class ...X>
-	struct is_all_same: std::integral_constant<bool, is_all_same<T, U>::value &
-                                        is_all_same<U, X...>::value>
-	{};
+	struct is_all_same: std::integral_constant<bool, is_all_same<T, U>::value & is_all_same<U, X...>::value> {};
 
-	template<class T, class U>
-	struct is_all_same<T, U>: std::false_type {};
+	template<class T, class U>	struct is_all_same<T, U>: std::false_type {};
  
-	template<class T>
-	struct is_all_same<T, T>: std::true_type {};
+	template<class T> struct is_all_same<T, T>: std::true_type {};
 	
 	template<class T, class ...X>
 	auto implement_array(X &&...args)-> std::array<T, sizeof...(args)>
@@ -57,8 +97,7 @@ namespace maths
 	}
 
 	template<class T, class ...U>
-	auto make_array(T && a, U &&...args)-> typename std::enable_if<is_all_same<T,U...>::value,
-                                        std::array<T, 1 + sizeof...(args)>>::type
+	auto make_array(T && a, U &&...args)-> typename std::enable_if<is_all_same<T,U...>::value, std::array<T, 1 + sizeof...(args)>>::type
 	{
 		return implement_array<T>(std::forward<T>(a), std::forward<U>(args)...);
 	}
@@ -68,15 +107,29 @@ namespace maths
 		return implement_array<T> (std::forward<T>(arg));
 	}
 	
-	template<class T>
-	T add(T a){
-		return a;
-	}
+	template<class T> T add(T a){ return a; }
 	
 	template<class T, typename ...U>
 	T add(T a, U ...b){
-		return (add(a) + add(b...));
+		return (a + add(b...));
 	}
+	
+	template<typename T>
+	typename std::remove_reference<T>::type &&move(T &&arg)
+	{
+		return static_cast<typename std::remove_reference<T>::type &&>(arg);
+	}
+	template<typename ...U>
+	std::tuple<U...> make_tuple(U &&...args)
+	{
+		return std::tuple<U...>(std::forward<U>(args)...);
+	}
+	template<typename T, typename...U>
+    std::shared_ptr<T> make_shared(U && ...args)
+    {
+        return std::shared_ptr<T>(new T (std::forward<U>(args)...));
+    }
+    
 	
 	constexpr double PI = 3.141;
 	std::string to_roman(int number)
@@ -145,36 +198,37 @@ namespace maths
 	
 	bool is_greater(const int &first, const int &second)
 	{
-		return first > second;
+		return !Less()(first, second);
 	}
 	bool is_lesser(const int &first, const int &second)
 	{
-		return !(is_greater(first, second));
+		return Less()(first, second);
 	}
 	
 	ivec generate_prime(const int &start, const int &end)
 	{
 		ivec vect_t {};
-		int starter {};
-		if(is_greater(start, end)){
-			vect_t.resize(start);
-			starter = end;
-		} else {
-			vect_t.resize(end);
-			starter = start;
+		if(is_greater(start, end)) return vect_t;
+		for(int i = start; i != end; ++i){
+			if(is_prime(i)){
+				vect_t.push_back(i);
+			}
 		}
-		std::generate(vect_t.begin(), vect_t.end(), [&]() mutable {return starter++; });
-		vect_t.erase(std::stable_partition(vect_t.begin(), vect_t.end(), is_prime), vect_t.end());
 		return vect_t;
 	}
-	/* generate multiplication table
-	svec generate_table(const std::initializer_list<int> &begin, const std::initializer_list<int> &end)
+	//generate multiplication table
+	std::vector<std::pair<std::string, int>> generate_table(const int &start, const int &column, const int &row)
 	{
-		svec vect_t {};
-		
+		std::string os { };
+		std::vector<std::pair<std::string, int>> vect_t {};
+		for(int x = start; x <= row; ++x){
+			for(int y = 1; y <= column; ++y){
+				os = std::to_string(x) + std::string(" x ") + std::to_string(y) + std::string(" = ");
+				vect_t.push_back(std::make_pair(os, x * y));
+			}
+		}
 		return vect_t;
 	}
-	*/
 	
 	template<class T, class ...U>
 	auto apply(T && a, U &&...b) -> decltype(a(b...)){
